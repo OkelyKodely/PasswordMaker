37b63b056f768ea44d0a5d54d1e3da8dc9224454
Daniel Cho <dh.cho428@gmail.com>
2019-09-13 23:52:59 -0700

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,87 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(13, 53)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(259, 45)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Passwords, Create"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.Simple
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.ItemHeight = 13
+        Me.ComboBox1.Location = New System.Drawing.Point(13, 104)
+        Me.ComboBox1.MaxDropDownItems = 100
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(259, 150)
+        Me.ComboBox1.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(13, 34)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(79, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Passwords Len"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(99, 26)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 3
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox1 As TextBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,57 @@
+﻿Public Class Form1
+
+    Private password As String
+
+    Private rd As New Random
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ComboBox1.Items.Clear()
+
+        For i = 0 To 100
+            password = ""
+            For x = 0 To CInt(TextBox1.Text) - 1
+
+                Dim capitalize = rd.Next(2)
+                If capitalize = 0 Then
+                    password = password & GetIt(rd.Next(10))
+                Else
+                    password = password & GetIt(rd.Next(10)).ToUpper
+
+                End If
+
+            Next
+            ComboBox1.Items.Add(password)
+
+
+        Next
+    End Sub
+
+    Private Function GetIt(ByVal number As Integer) As String
+
+        Select Case number
+            Case 0
+                Return "a"
+            Case 1
+                Return "b"
+            Case 2
+                Return "c"
+            Case 3
+                Return "d"
+            Case 4
+                Return "e"
+            Case 5
+                Return "f"
+            Case 6
+                Return "g"
+            Case 7
+                Return "h"
+            Case 8
+                Return "i"
+            Case 9
+                Return "j"
+        End Select
+
+        Return ""
+
+    End Function
+
+End Class
